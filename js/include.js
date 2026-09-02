1dfcda578b59970a4c4ff4d3f52603ce24a1984c
Triggertechfinserv <kiranrathva860@gmail.com>
2026-09-02 20:21:14 +0530

    Implement auto-update for footer copyright year
    Add script to auto-update footer copyright year.

--- js/include.js
+++ js/include.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+    // Auto-update footer copyright year
+    const yearElement = document.getElementById("year");
+
+    if (yearElement) {
+        yearElement.textContent = new Date().getFullYear();
+    }
+
+});
